a6be445e0571dbfc6eea861f34b4d2e8f0e5ecbe
heeheepark <pjh9641@gmail.com>
2026-09-14 05:24:45 +0200

    chore: 프로그램 초기 셋팅

---
+++ src/main/java/org/example/jupjupscheduler/JupjupSchedulerApplication.java
@@ -0,0 +1,13 @@
+package org.example.jupjupscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JupjupSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JupjupSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jupjupscheduler/JupjupSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jupjupscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupjupSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
