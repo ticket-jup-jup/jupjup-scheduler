bae14654f576366b0dcae3548ea13aff5214c11a
heeheepark <pjh9641@gmail.com>
2026-09-14 05:59:44 +0200

    chore: Dockerfile 추가

--- src/test/java/org/example/jupjupscheduler/JupjupSchedulerApplicationTests.java
+++ src/test/java/org/example/jupjupscheduler/JupjupSchedulerApplicationTests.java
@@ -1,13 +1,17 @@
 package org.example.jupjupscheduler;
 
+import org.example.jupjupscheduler.client.JupjupApiClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class JupjupSchedulerApplicationTests {
 
+    @MockitoBean
+    private JupjupApiClient jupjupApiClient;
+
     @Test
     void contextLoads() {
     }
-
 }
