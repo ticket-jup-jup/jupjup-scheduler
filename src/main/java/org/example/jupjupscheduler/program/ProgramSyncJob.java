1b8e4e0d35eae1d0a0cfb0055ac096c6a3a6e2af
heeheepark <pjh9641@gmail.com>
2026-09-14 13:49:23 +0200

    feat: 태스크 종료 로직 추가

--- src/main/java/org/example/jupjupscheduler/program/ProgramSyncJob.java
+++ src/main/java/org/example/jupjupscheduler/program/ProgramSyncJob.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.jupjupscheduler.client.JupjupApiClient;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,13 +14,21 @@ import org.springframework.stereotype.Component;
 public class ProgramSyncJob implements CommandLineRunner {
 
     private final JupjupApiClient jupjupApiClient;
+    private final ConfigurableApplicationContext context;
 
     @Override
     public void run(String... args) {
         log.info("프로그램 동기화 시작");
 
-        jupjupApiClient.syncPrograms();
+        try {
+            jupjupApiClient.syncPrograms();
+            log.info("프로그램 동기화 성공");
 
-        log.info("프로그램 동기화 종료");
+            SpringApplication.exit(context, () -> 0);
+        } catch (Exception e) {
+            log.error("프로그램 동기화 실패", e);
+
+            SpringApplication.exit(context, () -> 1);
+        }
     }
 }
