80d841259a6daf16713c742b8b7012944cb14908
heeheepark <pjh9641@gmail.com>
2026-09-14 19:38:04 +0200

    feat: test 코드 수정

--- src/test/java/org/example/jupjupscheduler/JupjupSchedulerApplicationTests.java
+++ src/test/java/org/example/jupjupscheduler/JupjupSchedulerApplicationTests.java
@@ -1,6 +1,7 @@
 package org.example.jupjupscheduler;
 
 import org.example.jupjupscheduler.client.JupjupApiClient;
+import org.example.jupjupscheduler.job.SchedulerJob;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -10,6 +11,9 @@ class JupjupSchedulerApplicationTests {
 
     @MockitoBean
     private JupjupApiClient jupjupApiClient;
+
+    @MockitoBean
+    private SchedulerJob schedulerJob;
 
     @Test
     void contextLoads() {
